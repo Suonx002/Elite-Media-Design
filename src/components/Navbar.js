33730996197fd1ea971c89ba0f64aee77f5e8267
Vuthy <suonx002@gmail.com>
2020-05-08 14:42:51 -0500

    adding navbar for desktop version

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,46 @@
 import React from 'react';
 
+import { AppBar, Toolbar, IconButton, Button } from '@material-ui/core';
+import useStyles from '../styles/NavbarStyles';
+
+import EliteMediaDesignLogo from '../assets/eliteMediaDesign.svg';
+
 const Navbar = () => {
-  return <div>navbar</div>;
+  const classes = useStyles();
+
+  return (
+    <>
+      <AppBar position='fixed' className={classes.appbar}>
+        <Toolbar>
+          <IconButton className={classes.logoContainer}>
+            <img
+              src={EliteMediaDesignLogo}
+              alt='logo'
+              className={classes.logo}
+            />
+          </IconButton>
+          <ul className={classes.rightMenuContainer}>
+            <li className={classes.rightMenuItem}>
+              <Button color='primary' className={classes.rightMenuItemBtn}>
+                About
+              </Button>
+            </li>
+            <li className={classes.rightMenuItem}>
+              <Button color='primary' className={classes.rightMenuItemBtn}>
+                Services
+              </Button>
+            </li>
+            <li className={classes.rightMenuItem}>
+              <Button color='primary' className={classes.rightMenuItemBtn}>
+                Contact
+              </Button>
+            </li>
+          </ul>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.marginToolbar} />
+    </>
+  );
 };
 
 export default Navbar;
---
+++ src/styles/NavbarStyles.js
@@ -0,0 +1,31 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+  marginToolbar: {
+    ...theme.mixins.toolbar,
+  },
+  appbar: {
+    backgroundColor: theme.palette.common.white,
+  },
+  logoContainer: {
+    height: 75,
+    width: 75,
+    padding: 0,
+  },
+  logo: {
+    width: '100%',
+    height: '100%',
+  },
+  rightMenuContainer: {
+    padding: 0,
+    marginLeft: 'auto',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  rightMenuItem: {
+    listStyle: 'none',
+    '&:not(:last-child)': {
+      marginRight: '0.75rem',
+    },
+  },
+}));
