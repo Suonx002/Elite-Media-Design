24ed5d0686be5b753e1c3776fff28af5662c7bad
Vuthy <suonx002@gmail.com>
2020-05-08 14:01:08 -0500

    init app and removing unnecessary files

---
+++ src/styles/Theme.js
@@ -0,0 +1,15 @@
+import { createMuiTheme } from '@material-ui/core';
+
+const primaryColor = '#002f6c';
+const secondaryColor = '#F0AD4E';
+
+export default createMuiTheme({
+  palette: {
+    primary: {
+      main: primaryColor,
+    },
+    secondary: {
+      main: secondaryColor,
+    },
+  },
+});
